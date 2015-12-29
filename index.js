4827750d538e162f45c21b6fbc315798c3cec158
sunny830321 <sunny830321@gmail.com>
2015-12-29 07:52:40 +0000

    update files

--- index.js
+++ index.js
@@ -5,25 +5,22 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
- io.on('connection', function(socket){
+
+io.on('connection', function(socket){
   console.log('a user connected');
 
   socket.broadcast.emit('hi');
-  
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
-});
+  });
 
-   socket.on('chat message', function(msg){
+
+  socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-
-});
-});
-  
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  });
 });
 
-http.listen(process.env.PORT || 3000, function(){
-     console.log('listening on *:3000');
+http.listen(process.env.PORT||3000, function(){
+  console.log('listening on *:3000');
 });
